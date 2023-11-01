b66057b3076475b0912f5416972cc625e3d8597d
Ehsan <ctech1699@gmail.com>
2023-11-02 01:50:06 +0530

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import Filterbar from './components/Filterbar'
 import Main from './components/Main'
 
 const App = () => {
   return (<>
 
-    {/* <Filterbar/> */}
+   
    <div>
    <Main/>
    </div>
